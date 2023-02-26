eefbe3913e9994adfb7c278e31f51af031b1394d
iikyc <karamsh2@hotmail.com>
2023-02-26 22:11:19 +0400

    Complete redesign of timeline page
    Removed dependence on csv lib, data processing through Pandas only

--- webapp/pages/03_Timeline.py
+++ webapp/pages/03_Timeline.py
@@ -1,108 +1,148 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import re
-import csv
-import sqlite3
-import time
+from datetime import datetime
 
-##
-# Page setup
-st.title("Incidents Timeline")
-st.sidebar.subheader("Visualization Options")
-graph_data_source = st.sidebar.selectbox("Data source", ("Logs", "Web Activity"))
-graph_type = st.sidebar.selectbox("Graph type", ("Bar Chart", "Line Graph"))
+# Page config
+st.set_page_config(layout="wide")
 
-global df
 
-##
-# Regular expressions
-date_regexp = "^[a-zA-Z]{3}\s{2}[0-9]"
-time_regexp = "[0-9]{2}:[0-9]{2}:[0-9]{2}"
-incident_regexp = "(?<=:\W)[a-zA-Z0-9\W]+(?=\n)"
-try:
-    ##
-    # Incidents file
-    # Transform auth_log.txt to a csv file
-    with open('incidents.csv', mode='w') as incidents_file:
-        auth_log_file = open(f"{st.session_state.path}/logs/auth_log.txt", "r")
-        fieldnames = ['date', 'time', 'incident']
-        writer = csv.DictWriter(incidents_file, fieldnames=fieldnames)
-        writer.writeheader()
-        for line in auth_log_file:
-            writer.writerow({'date': re.search(date_regexp, line).group(0), 'time': re.search(time_regexp, line).group(0), 'incident': re.search(incident_regexp, line).group(0)})
+## START TEST
 
-    main_log_file = open("incidents.csv", "r")
-    get_firefox_history = "SELECT * FROM moz_places"
-    get_chrome_history = "SELECT * FROM urls"
+# Open the dpkg.log file and read its contents
+with open(f'{st.session_state.path}/logs/last.txt', 'r') as f:
+    contents = f.read()
 
-    df = pd.read_csv(main_log_file)
-    df = df.set_index("time")
+# Split the contents into lines
+lines = contents.split('\n')
 
-    ##
-    # Outputting the graphs
-    if graph_type == "Bar Chart" and graph_data_source == "Logs":
-        chart = st.bar_chart(df.groupby(['date']).count())
-    if graph_type == "Line Graph" and graph_data_source == "Logs":
-        chart = st.line_chart(df.groupby(['date']).count())
+# Create an empty list to store the data
+lastlog_data = []
 
-    left_column, right_column = st.columns(2)
+# Loop through the lines and extract the data
+for line in lines:
+    try:
+        time = re.findall("(?<=[0-9]\s)[0-9]{2}:[0-9]{2}", line)[0]
+        date_raw = re.findall("(?<=\s)[a-zA-Z]{3}\s+[0-9]{1,2}", line)[0]
+        date_obj = datetime.strptime(date_raw, "%b %d")
+        date = date_obj.strftime("2023-%m-%d")
+        event = line.split()[0] + " " + " ".join(line.split()[-2:])
+        type = "users"
+        lastlog_data.append([date, time, event, type])
+    except:
+        continue
 
-    ##
-    # Print the log file
-    with left_column:
-        st.info("Logs")
-        st.write(df)
+users_df = pd.DataFrame(lastlog_data, columns=['date', "time", "event", "type"])
 
-    ##
-    # Print web activity
-    with right_column:
-        st.info("Web Activity")
-        # Fetch Chrome browser details
-        firefox_connection = sqlite3.connect(f"{st.session_state.path}/browsers/firefox.sqlite")
-        firefox_cursor = firefox_connection.cursor()
-        firefox_cursor.execute(get_firefox_history)
-        firefox_sites = firefox_cursor.fetchall()
+## END TEST
+# Open the dpkg.log file and read its contents
+with open(f'{st.session_state.path}/logs/dpkg_log.txt', 'r') as f:
+    contents = f.read()
 
-        browser_history_df = pd.DataFrame(columns=['date', 'incident'])
+# Split the contents into lines
+lines = contents.split('\n')
 
-        # Creating a pandas dataframe from the sqlite database
-        for firefox_site in firefox_sites:
-            try:
-                if type(firefox_site[8]) == "NoneType":
-                    continue
-                else:
-                    browser_history_df = browser_history_df.append(
-                        {'date': time.strftime('%b %d', time.localtime(firefox_site[8])), 'incident': firefox_site[1]},
-                        ignore_index=True)
-            except Exception as e:
-                #st.write(e)
-                continue
-        # Close the connection
-        firefox_connection.close()
+# Create an empty list to store the data
+dpkg_data = []
 
-        # Fetch Chrome browser details
-        chrome_connection = sqlite3.connect(f"{st.session_state.path}/browsers/chrome.sqlite")
-        chrome_cursor = chrome_connection.cursor()
-        chrome_cursor.execute(get_chrome_history)
-        chrome_sites = chrome_cursor.fetchall()
+# Loop through the lines and extract the data
+for line in lines:
+    if line.startswith('2023'):
+        parts = line.split()
+        date = parts[0]
+        time = parts[1]
+        event = parts[2] + parts[3] + parts[4]
+        type = "dpkg.log"
+        dpkg_data.append([date, time, event, type])
 
-        # Appending to the previous dataframe
-        for chrome_site in chrome_sites:
-            browser_history_df = browser_history_df.append(
-                {'date': time.strftime('%b %d', time.localtime(chrome_site[5])), 'incident': chrome_site[1]},
-                ignore_index=True)
+# Create a Pandas dataframe from the data
+dpkglog_df = pd.DataFrame(dpkg_data, columns=['date', "time", "event", "type"])
 
-        # Close the connection
-        chrome_connection.close()
-        # Print the dataframe
-        st.write(browser_history_df)
+# Open the auth.log file and read its contents
+with open(f'{st.session_state.path}/logs/auth_log.txt', 'r') as f:
+    contents = f.read()
 
-    ##
-    # Outputting the graphs
-    if graph_type == "Bar Chart" and graph_data_source == "Web Activity":
-        chart = st.bar_chart(browser_history_df.groupby(['date']).count())
-    if graph_type == "Line Graph" and graph_data_source == "Web Activity":
-        chart = st.line_chart(browser_history_df.groupby(['date']).count())
+# Split the contents into lines
+lines = contents.split('\n')
 
-except FileNotFoundError:
-    st.error("No data source specified")
+# Create an empty list to store the data
+auth_data = []
+
+time_regex = "[0-9]{2}:[0-9]{2}:[0-9]{2}"
+
+# Loop through the lines and extract the data
+for line in lines:
+    try:
+        date_obj = datetime.strptime(line[:6], "%b %d")
+        date = date_obj.strftime("2023-%m-%d")
+    except:
+        continue
+    try:
+        time = re.findall(time_regex, line)[0]
+    except:
+        continue
+    event = line[26:]
+    type = "auth.log"
+    auth_data.append([date, time, event, type])
+
+# Create a Pandas dataframe from the data
+authlog_df = pd.DataFrame(auth_data, columns=["date", "time", "event", "type"])
+total_df = authlog_df.append(dpkglog_df, ignore_index=True)
+total_df = total_df.append(users_df, ignore_index=True)
+
+# Chart placeholder element
+timeline_chart = st.empty()
+
+# Search bar
+st.subheader("Search")
+search_query = st.text_input("")
+# Downloads total_df as a .csv file
+st.download_button("Download", total_df.to_csv(index=False).encode('utf-8'), "seeker_timline.csv", "text/csv", key='download-csv')
+
+# Columns
+col1, col2 = st.columns(2)
+
+# Left column
+with col1:
+    st.subheader("Options")
+    st.selectbox("Chart Type", ["Bar Chart", "Line Chart"])
+    source_selection = st.selectbox("Source", ["All", "auth.log", "dpkg.log", "users"])
+    
+    if source_selection == "All":
+        with timeline_chart:
+            st.bar_chart(total_df.groupby(['date']).size())
+    if source_selection == "auth.log":
+        with timeline_chart:
+            auth_df = total_df.loc[total_df['type'] == "auth.log"]
+            st.bar_chart(auth_df.groupby(['date']).size())
+    if source_selection == "dpkg.log":
+        with timeline_chart:
+            dpkg_df = total_df.loc[total_df['type'] == "dpkg.log"]
+            st.bar_chart(dpkg_df.groupby(['date']).size())
+
+# Right column
+with col2:
+    events_dataframe = st.empty()
+    if source_selection == "All":
+        with events_dataframe:
+            st.dataframe(total_df, width=None, height=None)
+        if search_query:
+            search_results_df = total_df.loc[total_df['event'].str.contains(search_query, case=False)]
+            with events_dataframe:
+                st.dataframe(search_results_df)
+    if source_selection == "auth.log":
+        with events_dataframe:
+            auth_df = total_df.loc[total_df['type'] == "auth.log"]
+            st.dataframe(auth_df, width=None, height=None)
+        if search_query:
+            search_results_df = auth_df.loc[auth_df['event'].str.contains(search_query, case=False)]
+            with events_dataframe:
+                st.dataframe(search_results_df)
+    if source_selection == "dpkg.log":
+        with events_dataframe:
+            dpkg_df = total_df.loc[total_df['type'] == "dpkg.log"]
+            st.dataframe(dpkg_df, width=None, height=None)
+        if search_query:
+            search_results_df = dpkg_df.loc[dpkg_df['event'].str.contains(search_query, case=False)]
+            with events_dataframe:
+                st.dataframe(search_results_df)
